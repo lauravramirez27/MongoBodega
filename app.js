6965e2b2a461b901ff7e63ff35a1092b0409a107
LauraRamirezCampus <lauravramirez0327@gmail.com>
2023-08-18 10:42:41 -0500

    feat: :white_check_mark: Se agrega generador de token
    Se agrega endpoint generador de token y validador  en cada endpoint

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ import express from "express";
 import appBodega from "./routers/bodegas.js";
 import appProducto from "./routers/producto.js";
 import {appInventario} from "./routers/inventarios.js";
+import { appToken,appVerify } from "./middlware/token.js";
 
 
 dotenv.config();
 let app = express();
 
 app.use(express.json());
-app.use("/Bodegas",appBodega);
-app.use("/Productos",appProducto);
-app.use("/Inventario",appInventario);
+app.use("/Bodega",appVerify,appBodega);
+app.use("/Productos",appVerify,appProducto);
+app.use("/Inventario",appVerify,appInventario);
+app.use("/token",appToken);
 
 let config = JSON.parse(process.env.MY_SERVER)
 
