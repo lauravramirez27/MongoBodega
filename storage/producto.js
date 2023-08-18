a419da9afbcf9b7c4ea3e4eea610365a51309262
LauraRamirezCampus <lauravramirez0327@gmail.com>
2023-08-18 10:04:24 -0500

    feat: :white_check_mark: Actualizacion de DTO
    Se modifica la clase productos para verificar datos

--- storage/producto.js
+++ storage/producto.js
@@ -16,7 +16,7 @@ import { IsDefined } from 'class-validator';
   "estado": 1,
   "created_by": 12
  */
-export class DProductos {
+export class Productos {
     constructor(p1, p2, p3, p4, p5) {
         this.id = p1;
         this.nombre = p2;
@@ -29,24 +29,24 @@ __decorate([
     Expose({ name: 'id' }),
     IsDefined({ message: () => { throw { status: 422, message: 'El id es obligatorio' }; } }),
     __metadata("design:type", Number)
-], DProductos.prototype, "id", void 0);
+], Productos.prototype, "id", void 0);
 __decorate([
     Expose({ name: 'nombre' }),
     IsDefined({ message: () => { throw { status: 422, message: 'El nombre es obligatorio y de tipo string' }; } }),
     __metadata("design:type", String)
-], DProductos.prototype, "nombre", void 0);
+], Productos.prototype, "nombre", void 0);
 __decorate([
     Expose({ name: 'descripcion' }),
     IsDefined({ message: () => { throw { status: 422, message: 'la descripcion es obligatorio y de tipo string' }; } }),
     __metadata("design:type", String)
-], DProductos.prototype, "descripcion", void 0);
+], Productos.prototype, "descripcion", void 0);
 __decorate([
     Expose({ name: 'estado' }),
     IsDefined({ message: () => { throw { status: 422, message: 'el estado es obligatorio y de tipo string' }; } }),
     __metadata("design:type", Number)
-], DProductos.prototype, "estado", void 0);
+], Productos.prototype, "estado", void 0);
 __decorate([
     Expose({ name: 'created_by' }),
     IsDefined({ message: () => { throw { status: 422, message: 'El created_by es obligatorio' }; } }),
     __metadata("design:type", Number)
-], DProductos.prototype, "created_by", void 0);
+], Productos.prototype, "created_by", void 0);
--- storage/producto.ts
+++ storage/producto.ts
@@ -9,7 +9,7 @@ import { IsDefined } from 'class-validator';
   "created_by": 12
  */
 
-export class DProductos {
+export class Productos {
     @Expose({ name: 'id' })
     @IsDefined({ message: () => { throw { status: 422, message: 'El id es obligatorio' } } })
     id: number;
@@ -25,7 +25,7 @@ export class DProductos {
     @Expose({ name: 'estado' })
     @IsDefined({ message: () => { throw { status: 422, message: 'el estado es obligatorio y de tipo string' } } })
     estado: number;
-
+    
     @Expose({ name: 'created_by' })
     @IsDefined({ message: () => { throw { status: 422, message: 'El created_by es obligatorio' } } })
     created_by: number;
